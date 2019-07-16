adc1b77bd482fe604741b13966df0cc6b6d5d4af
Stephane Thomin <thomin.stephane@gmail.com>
2019-07-16 12:07:26 +0200

    solution java8 interface

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -9,6 +9,11 @@ public class Eagle extends Bird {
         this.altitude = 0;
     }
 
+    @Override
+    public String sing() {
+        return "Screech!";
+    }
+
     public int getAltitude() {
         return altitude;
     }
@@ -17,8 +22,54 @@ public class Eagle extends Bird {
         return flying;
     }
 
+    /**
+     * flies, must be on ground
+     */
     @Override
-    public String sing() {
-        return "Screech!";
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+    /**
+     * ascend
+     * @param meters altitude increase
+     * @return altitude
+     */
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * descend
+     * @param meters altitude decrease
+     * @return altitude
+     */
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * lands on the ground
+     */
+    @Override
+    public void land() {
+        if (this.flying && this.altitude == 1) {
+            System.out.printf("%s land of the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't lands.%n", this.getName());
+        }
     }
 }
